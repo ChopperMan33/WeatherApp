a3ef742c9ccb7ad422d0b000ebdaa699d44e9113
吴一颖 <wuyiying@wuyiyingdeMacBook-Pro.local>
2018-10-07 11:25:12 +0800

    init project

---
+++ app/src/main/java/com/example/wuyiying/weather01/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.wuyiying.weather01;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity{
+    @Override
+    protected void onCreate(Bundle saveInstanceState){
+
+        super.onCreate(saveInstanceState);
+        setContentView(R.layout.weather_info);
+    }
+}
