d70403234e223bd10db0084dd54ad91510b569bd
吴一颖 <wuyiying@wuyiyingdeMacBook-Pro.local>
2018-11-02 16:28:46 +0800

    12

--- app/src/main/java/com/example/wuyiying/weather01/SelectCity.java
+++ app/src/main/java/com/example/wuyiying/weather01/SelectCity.java
@@ -7,11 +7,12 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.AdapterView;
 import android.util.Log;
-
+import android.widget.EditText;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -19,14 +20,19 @@ import java.util.List;
 import wuyiyng.bean.City;
 import app.MyApplication;
 
+
 public  class SelectCity extends AppCompatActivity implements View.OnClickListener {
 
     private ImageView mBackBtn;
     private ListView citylist;
     private List<City> mCityList;
+    private EditText searchEt;
+    private ImageView searchBtn;
+
 
     private MyApplication mApplication;
     private ArrayList<String> mArrayList;
+
     private int updateCityCode = -1;
 
     @Override
@@ -37,16 +43,23 @@ public  class SelectCity extends AppCompatActivity implements View.OnClickListen
 
         mBackBtn = (ImageView) findViewById(R.id.title_back);
         mBackBtn.setOnClickListener(this);
+        //绑定组件
+        searchEt=(EditText)findViewById(R.id.select_search);
+        searchBtn=(ImageView)findViewById(R.id.search_button);
+        searchBtn.setOnClickListener(this); //点击事件
 
         //String[] listdata={"1","2","3"};
         mApplication = (MyApplication) getApplication();
         mCityList = mApplication.getCityList();
         mArrayList = new ArrayList<String>();
         for (int i = 0; i < mCityList.size(); i++) {
+            String No_=Integer.toString(i+1);
+            String number=mCityList.get(i).getNumber();
+            String  provinceName=mCityList.get(i).getProvince();
             String cityName = mCityList.get(i).getCity();
-            mArrayList.add(cityName);
+            mArrayList.add("NO."+No_+":"+number+"-"+provinceName+"-"+cityName);
         }
-        citylist = (ListView) findViewById(R.id.title_list);
+        citylist =  (ListView) findViewById(R.id.title_list);
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(SelectCity.this, android.R.layout.simple_list_item_1, mArrayList);
         citylist.setAdapter(adapter);
 
@@ -68,12 +81,37 @@ public  class SelectCity extends AppCompatActivity implements View.OnClickListen
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
-            case R.id.title_back:
-                Intent i = new Intent();
-                i.putExtra("cityCode", Integer.toString(updateCityCode));//"101160101" "101160101"
-                setResult(RESULT_OK, i);
+        //设置动作，将获取EditText获取的数据（这里我们获取的是citycode。
+            // 传给MainActivity更新天气数据
+            case R.id.search_button:
+                String citycode = searchEt.getText().toString();
+                Log.d("Search",citycode);
+                ArrayList<String> mSearvhList=new ArrayList<String>();
+                for (int i = 0; i < mCityList.size(); i++) {
+                    String No_=Integer.toString(i+1);
+                    String number=mCityList.get(i).getNumber();
+                    String  provinceName=mCityList.get(i).getProvince();
+                    String cityName = mCityList.get(i).getCity();
+                    if (number.equals(citycode)){
+                        mSearvhList.add("NO."+No_+":"+number+"-"+provinceName+"-"+cityName);
+                        Log.d("change adapter data","NO."+No_+":"+number+"-"+provinceName+"-"+cityName);
+                    }
+                    ArrayAdapter<String> adapter=new ArrayAdapter<String>(SelectCity.this,android.R.layout.simple_list_item_1,mSearvhList);
+                    citylist.setAdapter(adapter);
+                    adapter.notifyDataSetChanged();
+                    //mArrayList.add("NO."+No_+":"+number+"-"+provinceName+"-"+cityName);
+                }
+                Intent intent = new Intent(this,MainActivity.class);
+                intent.putExtra("cityCode",citycode);
+                setResult(RESULT_OK, intent);
                 finish();
-                break;
+
+//            case R.id.title_back:
+//                Intent i = new Intent();
+//                i.putExtra("cityCode", Integer.toString(updateCityCode));//"101160101" "101160101"
+//                setResult(RESULT_OK, i);
+//                finish();
+//                break;
             default:
                 break;
         }
